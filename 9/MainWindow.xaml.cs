19aa2f16a91d656aed4813837fbb7bcff0ef8710
Tomitruum <electrogames2124@gmail.com>
2025-05-07 13:06:36 +0500

    Добавьте файлы проекта.

---
+++ 9/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows;
+
+namespace TimeApp
+{
+    public partial class MainWindow : Window
+    {
+        Time vremya;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void vychestMinuty_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                byte chasy = byte.Parse(vvodChasov.Text);
+                byte minuty = byte.Parse(vvodMinut.Text);
+                uint skolko = uint.Parse(vvodVichMinut.Text);
+
+                vremya = new Time(chasy, minuty);
+                vremya = vremya - skolko;
+                rezultat.Text = "Новое время: " + vremya.ToString();
+            }
+            catch (Exception ex)
+            {
+                rezultat.Text = "Ошибка: " + ex.Message;
+            }
+        }
+
+        private void obnulytVse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                vremya = !vremya;
+                rezultat.Text = "Обнулено: " + vremya.ToString();
+            }
+            catch
+            {
+                rezultat.Text = "Ошибка: создайте время сначала.";
+            }
+        }
+
+        private void obnulytMinuty_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                vremya = --vremya;
+                rezultat.Text = "Обнулены минуты: " + vremya.ToString();
+            }
+            catch
+            {
+                rezultat.Text = "Ошибка: создайте время сначала.";
+            }
+        }
+
+        private void proverkaNaNol_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool estVremya = (bool)vremya;
+                rezultat.Text = estVremya ? "Время не ноль" : "Всё обнулено";
+            }
+            catch
+            {
+                rezultat.Text = "Ошибка: создайте время сначала.";
+            }
+        }
+
+        private void pokazatChasy_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                byte chasy = vremya;
+                rezultat.Text = "Часы (как byte): " + chasy;
+            }
+            catch
+            {
+                rezultat.Text = "Ошибка: создайте время сначала.";
+            }
+        }
+
+        private void sravnitRavno_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                byte chasy1 = byte.Parse(vvodChasov.Text);
+                byte minuty1 = byte.Parse(vvodMinut.Text);
+                byte chasy2 = byte.Parse(vvodChasov2.Text);
+                byte minuty2 = byte.Parse(vvodMinut2.Text);
+
+                Time vremya1 = new Time(chasy1, minuty1);
+                Time vremya2 = new Time(chasy2, minuty2);
+
+                bool result = vremya1 == vremya2;
+                rezultat.Text = "Результат == : " + result;
+            }
+            catch (Exception ex)
+            {
+                rezultat.Text = "Ошибка: " + ex.Message;
+            }
+        }
+
+        private void sravnitNeRavno_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                byte chasy1 = byte.Parse(vvodChasov.Text);
+                byte minuty1 = byte.Parse(vvodMinut.Text);
+                byte chasy2 = byte.Parse(vvodChasov2.Text);
+                byte minuty2 = byte.Parse(vvodMinut2.Text);
+
+                Time vremya1 = new Time(chasy1, minuty1);
+                Time vremya2 = new Time(chasy2, minuty2);
+
+                bool result = vremya1 != vremya2;
+                rezultat.Text = "Результат != : " + result;
+            }
+            catch (Exception ex)
+            {
+                rezultat.Text = "Ошибка: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ 9/Time.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TimeApp
+{
+    public class Time
+    {
+        public byte hours;
+        public byte minutes;
+
+        public Time(byte chasov, byte minut)
+        {
+            hours = chasov;
+            minutes = minut;
+        }
+
+        public Time vichestMinuty(uint skolko)
+        {
+            int vsegoMinut = hours * 60 + minutes - (int)skolko;
+            if (vsegoMinut < 0)
+            {
+                vsegoMinut = 24 * 60 + vsegoMinut % (24 * 60);
+            }
+            byte novChas = (byte)((vsegoMinut / 60) % 24);
+            byte novMin = (byte)(vsegoMinut % 60);
+            return new Time(novChas, novMin);
+        }
+
+        public static Time operator -(Time vremya, uint skolko)
+        {
+            return vremya.vichestMinuty(skolko);
+        }
+
+        public static Time operator !(Time vremya)
+        {
+            return new Time(0, 0);
+        }
+
+        public static Time operator --(Time vremya)
+        {
+            return new Time(vremya.hours, 0);
+        }
+
+        public static implicit operator byte(Time vremya)
+        {
+            return vremya.hours;
+        }
+
+        public static explicit operator bool(Time vremya)
+        {
+            return vremya.hours != 0 || vremya.minutes != 0;
+        }
+
+        public static bool operator ==(Time a, Time b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (a is null || b is null) return false;
+            return a.hours == b.hours && a.minutes == b.minutes;
+        }
+
+
+        public static bool operator !=(Time a, Time b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Time drugoe)
+                return this == drugoe;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return hours.GetHashCode() ^ minutes.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{hours:D2}:{minutes:D2}";
+        }
+    }
+}
